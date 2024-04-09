5a2ef9ac0741f31853b430a4fa2ce7b08f1652b8
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2024-04-08 22:07:15 -0700

    Worked On Workout Page
    Created data types needed for the workout page and components. Sets, exercise, workout data type/interfaces. Also added a sample data to work with.

---
+++ src/app/create/workout/page.tsx
@@ -0,0 +1,103 @@
+'use client';
+import React from 'react'
+import '@/styles/workout.css'
+import { WorkoutView } from '@/components/Workout/WorkoutComponents'
+
+interface SetData {
+   setNumber: number
+   reps: number
+   weight: number
+}
+
+interface ExerciseData {
+   exerciseName: string
+   sets: SetData[]
+}
+
+interface WorkoutData {
+   workoutName: string
+   notes?: string
+   totalReps?: number
+   totalVolume?: number
+   exercises: ExerciseData[]
+}
+
+
+let sampleWorkoutData: WorkoutData = {
+   workoutName: 'Chest Day',
+   notes: 'This is a sample workout for chest day',
+   exercises: [
+      {
+         exerciseName: 'Bench Press',
+         sets: [
+            {
+               setNumber: 1,
+               reps: 10,
+               weight: 135
+            },
+            {
+               setNumber: 2,
+               reps: 8,
+               weight: 185
+            },
+            {
+               setNumber: 3,
+               reps: 6,
+               weight: 225
+            }
+         ]
+      },
+      {
+         exerciseName: 'Incline Bench Press',
+         sets: [
+            {
+               setNumber: 1,
+               reps: 10,
+               weight: 95
+            },
+            {
+               setNumber: 2,
+               reps: 8,
+               weight: 115
+            },
+            {
+               setNumber: 3,
+               reps: 6,
+               weight: 135
+            }
+         ]
+      },
+      {
+         exerciseName: 'Dumbbell Flys',
+         sets: [
+            {
+               setNumber: 1,
+               reps: 10,
+               weight: 20
+            },
+            {
+               setNumber: 2,
+               reps: 8,
+               weight: 25
+            },
+            {
+               setNumber: 3,
+               reps: 6,
+               weight: 30
+            }
+         ]
+      }
+   ]
+}
+
+
+
+function page() {
+   return (
+      <div>
+         <WorkoutView workoutData={sampleWorkoutData} />
+      </div>
+   );
+}
+
+export default page
