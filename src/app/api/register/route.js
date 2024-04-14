76504aa39bd55a9f194afe48d77d409261fab0fb
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2024-04-13 20:58:50 -0700

    Fixed Import User Not Being Capitalized

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,6 +1,6 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import { NextResponse } from "next/server";
-import User from "@/models/user";
+import User from "@/models/User";
 import bcrypt from "bcryptjs";
 
 export async function POST(req) {
