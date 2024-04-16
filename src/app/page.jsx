3b6797febe9684df5ae0fba6b098807bd7ba4846
HimayRadec <80723658+HimayRadec@users.noreply.github.com>
2024-04-15 23:59:12 -0700

    Update page.jsx
    Font size differs on deployment server vs local dev. So I am shrinking it.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <div className="text-center">
-        <h1 className="text-5xl mb-1 lg:text-6xl">
+        <h1 className="text-4xl mb-1 lg:text-6xl">
           PUMP STATS
         </h1>
         <h1>Coming Soon</h1>
